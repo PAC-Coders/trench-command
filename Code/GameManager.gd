2af9a1ed9b267031b4c8d62d11c7505f4f414bf2
earldoom <67607843+earldoom@users.noreply.github.com>
2025-09-09 13:15:38 +0400

    Improve pause menu and crosshair visibility handling
    Updated GameManager to show/hide the crosshair and set mouse mode appropriately when pausing and resuming the game. Adjusted Crosshair and Player node scaling and process modes in scene files for better UI consistency.

--- Code/GameManager.gd
+++ Code/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var pause_menu = $"../CanvasLayer/PauseMenu"
+@onready var crosshair = $"../CanvasLayer/Crosshair"
 
 var game_paused: bool = false
 
@@ -8,12 +9,26 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		game_paused = not game_paused
 		
-	if game_paused == true :
+	if game_paused == true:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		crosshair.hide()
 		get_tree().paused = true
 		pause_menu.show()
-	else :
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		crosshair.show()
 		get_tree().paused = false
 		pause_menu.hide()
 
 func _on_resume_pressed() -> void:
 	game_paused = not game_paused
+	if game_paused == true :
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		crosshair.hide()
+		get_tree().paused = true
+		pause_menu.show()
+	else :
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		crosshair.show()
+		get_tree().paused = false
+		pause_menu.hide()
--- UI/Crosshair.gd
+++ UI/Crosshair.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 func _ready() -> void:
-    Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(delta: float) -> void:
-    global_position = get_viewport().get_mouse_position()
+	global_position = get_viewport().get_mouse_position()
